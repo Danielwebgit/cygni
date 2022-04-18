458447fd1e13af12b650772019fd6bebaac8392d
danielwebgit <nielferas@gmail.com>
2022-04-18 16:12:49 -0300

    Definido as migrations

---
+++ database/migrations/2022_04_18_154414_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('score',60);
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->integer('player_id')->unsigned();
+            $table->foreign('player_id')->references('id')->on('players');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\{
+    PlayerController,
+    ScoreController,
+    GameController
+};
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +20,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/** Rota do grupo de jogadores v1 */
+Route::prefix('jogadores')->group(function (){
+
+    Route::get('/', [PlayerController::class, 'index'])
+    ->name('player.index');
+
+    Route::post('/cadastrar-jogador', [PlayerController::class, 'store'])
+    ->name('player.store');
+});
+
+/** Rota do grupo de jogos v1 */
+Route::prefix('jogos')->group(function (){
+
+    Route::get('/', [GameController::class, 'index'])
+    ->name('game.index');
+
+    Route::post('/cadastrar-jogador', [GameController::class, 'store'])
+    ->name('game.store');
+});
+
+/** Rota do grupo de scores v1 */
+Route::prefix('scores')->group(function (){
+
+    Route::get('/', [ScoreController::class, 'index'])
+    ->name('player-score.index');
+
+    Route::post('/jogador/{player}', [ScoreController::class, 'store'])
+    ->name('player-score.store');
 });
